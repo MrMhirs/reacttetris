2c62440ea0bdf16a559fe8ee497e7a9f5829a848
RB <87038824+MrMhirs@users.noreply.github.com>
2023-02-20 12:51:54 +0100

    errors

---
+++ src/components/styles/StyledDisplay.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledDisplay = styled.div`
+    display: flex;
+    align-items: center;
+    border: 4px solid #333;
+    background: #000;
+    box-sizing: border-box;
+    
+
+`
